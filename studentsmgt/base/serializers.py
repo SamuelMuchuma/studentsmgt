9bd4a7a0247e889dfd03b659c8244498427a9d65
Sam <samuelmuchuma@gmail.com>
2022-08-23 12:04:40 +0300

    Auth Modification

--- studentsmgt/base/serializers.py
+++ studentsmgt/base/serializers.py
@@ -7,11 +7,11 @@ from django.contrib.auth.models import User
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(validated_data['email'], validated_data['email'], validated_data['password'])
 
         return user
 
